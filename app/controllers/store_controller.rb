ff19ba90b00d3ba5cca77c5e689db055d3cda8b1
PJ <yorko9999@gmail.com>
2015-04-14 16:13:05 +0100

    Chapter 11
    Added partials to store page, and began the Ajax process

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,9 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
+  
   def index
   	@products = Product.order(:title)
   	@time = Time.now.to_s(:long_ordinal)
-  end
+  end  
 end
